99bc585e5b577cf371a8aa5e2996146f02e4b085
Luiz Miguel <luizmiguelbelizariodasilva@gmail.com>
2021-10-06 15:14:41 -0300

    confirmar em 'main'

--- src/app.js
+++ src/app.js
@@ -9,6 +9,22 @@ const productRoute = require('./routes/product.routes');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+//
+app.use((req, res, next) => {
+    res.header('Acces-Control-Allow-Origin', 'http://localhost:8080/'); //Aqui onde seta o srv(endPont) espercifico no lugar do '*'. Esta API só seria acesivel deste srv.
+    res.header(//propriedade de cabeçalho aceita na API
+        'Acces-Control-Allow-Origin', 
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+     ); 
+
+     if (req.method === 'OPTIONS'){
+         res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+         return res.status(200).send({});
+     }
+
+     next();
+});
+//
 app.use(express.json({ type: 'application/vnd.api+json' }));
 app.use(cors());
 
