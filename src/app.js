43f82fc3fc9cd3360a479aece2e41da71299d764
Luiz Miguel <luizmiguelbelizariodasilva@gmail.com>
2021-10-11 15:16:30 -0300

    .envEXEMPLO - é no .env onde são setadas as credenciais de conecxão ao banco

--- src/app.js
+++ src/app.js
@@ -16,6 +16,7 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");//http://localhost:8080
     res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
     //res.header("Access-Control-Allow-Methods", 'GET,PUT,POST');
+    //res.header("Access-Control-Allow-Headers", "OPTIONS,Accept,Authorization, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Header");
     app.use(cors());
     next();
 });
