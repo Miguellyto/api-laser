cc90d78d6f5a6d25dccfbe95eec2a636ced50d75
Luiz Miguel <luizmiguelbelizariodasilva@gmail.com>
2021-10-27 15:44:39 -0300

    Update Query in ProductController

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -8,23 +8,40 @@ const moment = require('moment');
 //  Lista todos os products:
 exports.listAllProducts = async (req, res) => {
     const response = await db.query(
-      `SELECT 
-      chave, 
-      ean,
-      nome, 
-      codigo,
-      classe,   
-      fabricante, 
-      imagem, 
-      marca, 
-      prazogarantia, 
-      descricaogenerica,
-      altura,
-      largura,
-      comprimento,
-      pesbruunid,
-      prvenda 
-      FROM RECURSO ORDER BY codigo ASC`,
+      `select 
+      a.chave, 
+      a.ean,
+      '' as ncm,
+      a.nome, 
+      a.codigo,
+      'quantidade' as quantidade,
+      'preço especial' as preço_especial,
+      b.fator as preço,
+      a.descricaogenerica,
+      a.marca, 
+      'custo' as custo,
+      a.prazogarantia,
+      'observacao' as observacao,
+      'link produto' as link_produto,
+      'acessivel' as acessivel,
+      a.classe,   
+      'tempo manuseio' as tempo_manuseio,
+      'tempo fabricacao' as tempo_fabricacao,
+      a.comprimento as comprimento,
+      a.largura as largura,
+      a.altura as altura,
+      a.pesbruunid as peso,
+      'atributos' as atributos,
+      'fotos' as fotos,
+      'tabelas de preco' as tabela_preco,
+      'deposito' as deposito,
+      'variacoes' as variacoes
+      from RECURSO a 
+      join fator b
+      on a.chave = b.recursoorig
+      and b.tabfator = 462894
+      and b.fim is null
+      `,
       //'SELECT * FROM products ORDER BY nome DESC',
     );
     res.status(200).send(response.rows);
@@ -34,23 +51,40 @@ exports.listAllProducts = async (req, res) => {
   exports.findProductById = async (req, res) => {
     const sku = parseInt(req.params.id);
     const response = await db.query(
-      `SELECT 
-      chave, 
-      ean,
-      nome, 
-      codigo,
-      classe,   
-      fabricante, 
-      imagem, 
-      marca, 
-      prazogarantia, 
-      descricaogenerica,
-      altura,
-      largura,
-      comprimento,
-      pesbruunid,
-      prvenda
-      FROM RECURSO WHERE codigo = $1`, 
+      `select 
+      a.chave, 
+      a.ean,
+      '' as ncm,
+      a.nome, 
+      a.codigo,
+      'quantidade' as quantidade,
+      'preço especial' as preço_especial,
+      b.fator as preço,
+      a.descricaogenerica,
+      a.marca, 
+      'custo' as custo,
+      a.prazogarantia,
+      'observacao' as observacao,
+      'link produto' as link_produto,
+      'acessivel' as acessivel,
+      a.classe,   
+      'tempo manuseio' as tempo_manuseio,
+      'tempo fabricacao' as tempo_fabricacao,
+      a.comprimento as comprimento,
+      a.largura as largura,
+      a.altura as altura,
+      a.pesbruunid as peso,
+      'atributos' as atributos,
+      'fotos' as fotos,
+      'tabelas de preco' as tabela_preco,
+      'deposito' as deposito,
+      'variacoes' as variacoes
+      from RECURSO a 
+      join fator b
+      on a.chave = b.recursoorig
+      and b.tabfator = 462894
+      and b.fim is null
+      where codigo = $1`, 
       [sku],
     );
     res.status(200).send(response.rows);
--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -2,11 +2,11 @@ const router = require('express-promise-router')();
 const productController = require('../controllers/product.controller');
 
 
-//  Lista todos os product: (GET): localhost:5000/api/recurso
-router.get('/recurso', productController.listAllProducts);
+//  Lista todos os ProdutoS: (GET): localhost:5000/api/product
+router.get('/product', productController.listAllProducts);
 
 //  Seleciona Produto pelo Id: (GET): localhost:5000/api/product/:id
-router.get('/recurso/:id', productController.findProductById);
+router.get('/product/:id', productController.findProductById);
 
 /* // ==> Rota responsável por criar um novo 'Product': (POST): localhost:5000/api/product
 router.post('/product', productController.createProduct);
