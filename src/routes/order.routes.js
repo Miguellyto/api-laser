b329819cae9d6d5247aad94f49f94e6db7a15fe6
Luiz Miguel <luizmiguelbelizariodasilva@gmail.com>
2021-11-09 15:54:14 -0300

    Up order

--- src/routes/order.routes.js
+++ src/routes/order.routes.js
@@ -2,11 +2,11 @@ const router = require('express-promise-router')();
 //const { default: axios } = require('axios');
 const orderController = require('../controllers/order.controller');
 
-//  Lista todos os pedido: (GET): localhost:4000/api/order
-router.get('/order', orderController.listAllOrders);
-
 //  Seleciona pedido pelo Id: (GET): localhost:4000/api/order/:id
 router.get('/order/:id', orderController.findOrderById);
+
+//  Lista todos os pedido: (GET): localhost:4000/api/order
+router.get('/order', orderController.listAllOrders);
 
 // ==> Rota responsável por criar um novo pedido: (POST): localhost:4000/api/order
 // router.post('/order', orderController.createOrder);
