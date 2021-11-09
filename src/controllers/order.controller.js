9acc10ae11c4c4306324311b4f9a00f8e721dc79
Luiz Miguel <luizmiguelbelizariodasilva@gmail.com>
2021-11-09 15:51:53 -0300

    Atualizando o OrderRouters e OrderController

--- src/controllers/order.controller.js
+++ src/controllers/order.controller.js
@@ -1,131 +1,77 @@
-//const db = require("../config/database");
-//const fetch = require('node-fetch');
-const { default: axios } = require('axios');
+const db = require("../config/database");
+const request = require('request');
+// const axios = require("axios").default;
 
-// Get All Orders
-exports.listAllOrders = async (req, res) => {
-try {
-  const { data } = await axios.get('http://jsonplaceholder.typicode.com/users');
-  return res.send({ data });
-
-} catch (error) {
-  res.send({ error: error.message});
-}
-};
-
-// Get One Order For ID
+////Get One Order For ID ---OK
 exports.findOrderById = async (req, res) => {
   const { id } = req.params;
-  try {
-    const { data } = await axios.get(`http://jsonplaceholder.typicode.com/users/${id}`);
-    return res.send({ data });
+  /* const { id, title } = req.body; */
 
-  } catch (error) {
-    res.send({ error: error.message});
-  }
-  };
+  if (!req.params.id) { 
+    res.status(500); 
+    res.send({"Error": "No ID"}); 
+ } 
 
-  // Create Order PluggTO on DataBase
-exports.createOrder = async (req, res) => {
-  const { id_produto, quantidade } = req.body;
-  const { rows } = await db.query(
-    'INSERT INTO orders (id_produto, quantidade) VALUES ($1, $2)',
-    [id_produto, quantidade],
-  );
-
-  res.status(201).send({
-    message: 'Pedido Criado com Successo',
-    body: {
-      order: { id_produto, quantidade }
-      
-    },
-  });
-};
-///
-
-/* export default axios.create({
-  baseURL: "https://api.yelp.com/v3/businesses/search",
-  headers: {
-    Authorization:
-      "Bearer ..."
-  }
-}); */
-
-/* axios.defaults.baseURL = 'https://api.example.com';
-axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
-axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
-
-axios.defaults.headers.authorization = `Bearer ${token}`; */
-
-/* 
-////Get All Orders
-exports.listAllOrders = async (req, res) => {
-  const url = 'https://api.plugg.to/orders';
   const options = {
     method: 'GET',
+    // url: `https://api.plugg.to/orders/${id}`,
+    // url: "https://jsonplaceholder.typicode.com/posts/" + req.params.id, 
+    url: `https://jsonplaceholder.typicode.com/posts/${id}`,
     headers: {
       Accept: 'application/json',
       Authorization: 'Bearer {ACCESS_TOKEN}',
       'Content-Type': 'application/json'
     }
   };
-  fetch(url, options)
-  .then(res => res.json())
-  .then(json => console.log(json))
-  .catch(err => console.error('error:' + err));
+
+    request(options, (error, response, body) => { 
+
+        if (!error && response.statusCode == 200) { 
+            //obter dados do corpo ... por exemplo título+ID
+            const data = JSON.parse(body);
+            const title = data.title || '';
+            const id = data.id || '';
+
+            // Conn Postgresql
+            (async () => {
+
+              const { rows } = await db.query('INSERT INTO titles(id, title) values($1, $2)', 
+              [id, title])
+
+                  res.json({
+                  message: 'Pedido Criado com Successo',
+                  body: {
+                    order: { id, title }
+                  },
+                });
+                          
+            })().catch(err =>
+              setImmediate(() => {throw new err(err)})
+            )
+        } 
+    }
+); 
 };
 
-////Get One Order For ID
-exports.findOrderById = async (req, res) => {
-//const url = 'https://api.plugg.to/orders';
-const url = 'https://api.plugg.to/orders/plugg_order_id';
-const options = {
+// Get All Orders
+exports.listAllOrders = async () => {
+  const options = {
   method: 'GET',
+  url: 'http://jsonplaceholder.typicode.com/users',
   headers: {
     Accept: 'application/json',
     Authorization: 'Bearer {ACCESS_TOKEN}',
     'Content-Type': 'application/json'
   }
 };
-  fetch(url, options)
-  .then(res => res.json())
-  .then(json => console.log(json))
-  .catch(err => console.error('error:' + err));
-}; 
+request(options, (error, response, body) => {
+  if (error) throw new Error(error);
+
+  console.log(body);
+  
+});
+};
 
 ////Status Update Order
-exports.updateOrderById = async (req, res) => {  
-  const url = 'https://api.plugg.to/orders/plugg_id';
-  const options = {
-    method: 'PUT',
-    headers: {
-    Accept: 'application/json',
-    Authorization: 'Bearer {ACCESS_TOKEN}',
-    'Content-Type': 'application/json'
-  }
-};
-  fetch(url, options)
-  .then(res => res.json())
-  .then(json => console.log(json))
-  .catch(err => console.error('error:' + err));
-};
-
 
 ////----Order receipt confirmation----////
-exports.createOrder = async (req, res) => { 
-const url = 'https://api.plugg.to/orders/plugg_id_order';
-const options = {
-  method: 'PUT',
-  headers: {
-    Accept: 'application/json',
-    Authorization: 'Bearer {ACCESS_TOKEN}',
-    'Content-Type': 'application/json'
-  },
-  body: JSON.stringify({external: 'external id', ack: true})
-};
-  fetch(url, options)
-  .then(res => res.json())
-  .then(json => console.log(json))
-  .catch(err => console.error('error:' + err));
-};
- */
--- src/routes/order.routes.js
+++ src/routes/order.routes.js
@@ -1,24 +1,22 @@
 const router = require('express-promise-router')();
-const { default: axios } = require('axios');
+//const { default: axios } = require('axios');
 const orderController = require('../controllers/order.controller');
 
-//  Lista todos os orders: (GET): localhost:5000/api/order
-router.get('/orders', orderController.listAllOrders);
+//  Lista todos os pedido: (GET): localhost:4000/api/order
+router.get('/order', orderController.listAllOrders);
 
-//  Lista pedido pelo Id: (GET): localhost:5000/api/order/:id
+//  Seleciona pedido pelo Id: (GET): localhost:4000/api/order/:id
 router.get('/order/:id', orderController.findOrderById);
 
-// ==> Rota responsável por criar um novo pedido: (POST): localhost:5000/api/order
-router.post('/order', orderController.createOrder);
+// ==> Rota responsável por criar um novo pedido: (POST): localhost:4000/api/order
+// router.post('/order', orderController.createOrder);
 
-/* 
-//  Atualiza pedido pelo Id: (PUT): localhost:5000/api/order/:id
-router.put('/order/:id', orderController.updateOrderById);
+//  Atualiza pedido pelo Id: (PUT): localhost:4000/api/order/:id
+// router.put('/order/:id', orderController.updateOrderById);
 
-////-----////
-//  Excluir pedido pelo Id: (DELETE): localhost:5000/api/order/:id
-router.delete('/order/:id', orderController.deleteOrderById);
- */
+//  Excluir pedido pelo Id: (DELETE): localhost:4000/api/order/:id
+// router.delete('/order/:id', orderController.deleteOrderById);
+
 
 // module.exports = axios;
 module.exports = router;
