fa690299b8d613461ca252a195e6570739fa8e7d
Luiz Miguel <luizmiguelbelizariodasilva@gmail.com>
2021-11-17 16:41:41 -0300

    Criando Rotas para Pagamento, Atualizar Pedido, Status Faturado, Envio do Pedido

--- src/routes/order.routes.js
+++ src/routes/order.routes.js
@@ -2,27 +2,38 @@ const router = require('express-promise-router')();
 //const { default: axios } = require('axios');
 const orderController = require('../controllers/order.controller');
 
-//  Get order For Id end save on BD: (GET): localhost:5000/api/order/:id
+//  Seleciona pedido pelo Id: (GET): localhost:4000/api/order/:id
 router.get('/order/:id', orderController.findOrderById);
 
-//  Lista todos os order: (GET): localhost:5000/api/orders
+//  Lista todos os pedido: (GET): localhost:4000/api/order
 router.get('/orders', orderController.listAllOrders);
 
-//  Lista todos os orders for BD: (GET): localhost:5000/api/order_bd
+//  Lista todos os pedido: (GET): localhost:4000/api/order
 router.get('/orders_bd', orderController.listAllOrders_bd);
 
-//  Seleciona order pelo Id BD: (GET): localhost:5000/api/order_bd/:id
+//  Seleciona pedido pelo Id: (GET): localhost:4000/api/order/:id
 router.get('/order_bd/:id', orderController.findOrderById_bd);
 
-// ==> Rota responsável por criar um novo order: (POST): localhost:5000/api/order
+// Pagamento do Pedido: (GET): localhost:4000/api/order/:id
+router.get('/order/pgto/:id', orderController.findOrderPgto);
+
+// Atualiza pedido pelo Id: (PATCH / PUT): localhost:4000/api/order/:id
+router.patch('/order/:id', orderController.updateOrderById);
+
+// Envio do Status Faturado do Pedido pelo Id: (GET): localhost:4000/api/order/:id
+router.get('/order/status/:id', orderController.findOrderStatus);
+
+// Envio do Status Despachado - Envio do Pedido: (GET): localhost:4000/api/order/:id
+router.get('/order/shipping/:id', orderController.findOrderShipping);
+
+//  ==> Rota responsável por criar um novo pedido: (POST): localhost:4000/api/order
 // router.post('/order', orderController.createOrder);
 
-//  Atualiza order pelo Id: (PUT): localhost:5000/api/order/:id
+//  ==> Atualiza pedido pelo Id: (PUT): localhost:4000/api/order/:id
 // router.put('/order/:id', orderController.updateOrderById);
 
-//  Excluir order pelo Id: (DELETE): localhost:5000/api/order/:id
+//  ==> Excluir pedido pelo Id: (DELETE): localhost:4000/api/order/:id
 // router.delete('/order/:id', orderController.deleteOrderById);
-
 
 // module.exports = axios;
 module.exports = router;
