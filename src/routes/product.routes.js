e087a9b80c7bd0fb0272b483f9c96b25a7f3e4c1
Luiz Miguel <luizmiguelbelizariodasilva@gmail.com>
2022-04-18 10:08:46 -0300

    Update comentario

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -2,19 +2,19 @@ const router = require('express-promise-router')();
 const productController = require('../controllers/product.controller');
 
 
-//  Lista todos os ProdutoS: (GET): localhost:5000/api/product
+//  Lista todos os ProdutoS: (GET): localhost:5000/product
 router.get('/product', productController.listAllProducts);
 
-//  Seleciona Produto pelo Id: (GET): localhost:5000/api/product/:id
+//  Seleciona Produto pelo Id: (GET): localhost:5000/product/:id
 router.get('/product/:id', productController.findProductById);
 
-/* // ==> Rota responsável por criar um novo 'Product': (POST): localhost:5000/api/product
+/* // ==> Rota responsável por criar um novo 'Product': (POST): localhost:5000/product
 router.post('/product', productController.createProduct);
 
-//  Atualiza Produto pelo Id: (PUT): localhost:5000/api/product/:id
+//  Atualiza Produto pelo Id: (PUT): localhost:5000/product/:id
 router.put('/product/:id', productController.updateProductById);
 
-//  Excluir Produto pelo Id: (DELETE): localhost:5000/api/product/:id
+//  Excluir Produto pelo Id: (DELETE): localhost:5000/product/:id
 router.delete('/product/:id', productController.deleteProductById); */
 
 module.exports = router;
